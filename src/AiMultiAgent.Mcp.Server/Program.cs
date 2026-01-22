75ded5d5535ba7b6691678ad60e75364964e31d0
kiril <khorosko15@gmail.com>
2026-01-22 23:28:58 +0300

    feat: add docker compose

--- src/AiMultiAgent.Mcp.Server/Program.cs
+++ src/AiMultiAgent.Mcp.Server/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.AI;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Scalar.AspNetCore;
+using static System.Net.WebRequestMethods;
 
 const string McpPath = "/api/mcp";
 
@@ -35,12 +36,16 @@ builder.Services.Configure<RouteOptions>(options =>
     options.LowercaseQueryStrings = false;
 });
 
+var baseUri = builder.Configuration["API_URI"]
+              ?? Environment.GetEnvironmentVariable("API_URI")
+              ?? throw new InvalidOperationException("API_URI is not set");
+
 
 builder.Services.AddSseMcpClient(
     options => options.EndpointPath = McpPath,
      http =>
      {
-         http.BaseAddress = new Uri("https://localhost:7244");
+         http.BaseAddress = new Uri(baseUri);
          http.Timeout = Timeout.InfiniteTimeSpan; // ждать столько, сколько сервер отдаёт
                                                   // или: http.Timeout = TimeSpan.FromMinutes(10); // ждать максимум 10 минут
      }
